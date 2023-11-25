ca7d548c67d35360649695724c1cacfc105e2c7b
Pedro Henrique Botelho <114683497+Pedrobtlh@users.noreply.github.com>
2023-11-25 12:53:44 -0300

    Autenticação de Usuário, Atualização no Toughts

--- routes/toughtsRouter.js
+++ routes/toughtsRouter.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const ToughtsController = require("../controllers/ToughtController");
 
+//helpers
+const checkAuth = require("../helpers/auth").checkAuth;
+
+router.get("/add", checkAuth, ToughtsController.createTought);
+router.post("/add", checkAuth, ToughtsController.createToughtSave);
+router.get("/dashboard", checkAuth, ToughtsController.dashboard);
 router.get("/", ToughtsController.showToughts);
 
 module.exports = router;
